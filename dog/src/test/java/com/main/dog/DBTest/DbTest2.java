bc9e22431a6e89689305c754109a1252a01c7caa
lee8442 <014310@naver.com>
2018-05-17 15:11:36 +0900

    v1

---
+++ dog/src/test/java/com/main/dog/DBTest/DbTest2.java
@@ -0,0 +1,40 @@
+package com.main.dog.DBTest;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+//JUnit Runtime Class
+@RunWith(SpringJUnit4ClassRunner.class)
+// Spring context xml 파일들을 읽어오는 설정
+/*
+ * @ContextConfiguration(locations =
+ * {"file:src/main/resources/applicationContext.xml",
+ * "file:src/main/resources/mybatis/myBatis-config.xml"})
+ */
+// WebApplication 위에서 불러온 Context 가 Spring 이 구현하는 WebApplicationContext 컨테이너라는
+// 것을 명시
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
+@WebAppConfiguration
+public class DbTest2 {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+
+	@Test
+	// Amazon DB 연결 테스트
+	public void dbTest() throws Exception {
+		System.out.println("결과 : " + sqlSessionTemplate.selectOne("com.mybatis.test.selectCount"));
+	}
+}
